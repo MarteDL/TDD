aef2fcec6ea99bfdf4a9a6c00d2b8b6739883501
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-07-12 09:36:36 +0200

    cleaned up tests

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -2,54 +2,51 @@ require 'rspec'
 require './lib/calc'
 
 describe 'Calc' do
+
+  calc = Calc.new
+
   context 'when creating a calculator object' do
     it 'succeeds' do
-      calculator = Calc.new
-      expect(calculator.class).to eql(Calc)
-    end
-  end
-end
-
-describe 'Calculate' do
-  context "given '5 + 5'" do
-    it "returns '10'" do
-      calc = Calc.new
-      expect(calc.calculate('5 + 5')).to eql(10)
+      expect(calc).to be_kind_of(Calc)
     end
   end
 
-  context "given '5 + 0'" do
-    it "returns '5'" do
-      calc = Calc.new
-      expect(calc.calculate('5 + 5')).to eql(10)
+  describe '#calculate' do
+    context "given '5 + 5'" do
+      it "returns '10'" do
+        expect(calc.calculate('5 + 5')).to eql(10)
+      end
     end
-  end
 
-  context "given '5 - 4'" do
-    it "returns '1'" do
-      calc = Calc.new
-      expect(calc.calculate('5 - 4')).to eql(1)
+    context "given '5 + 0'" do
+      it "returns '5'" do
+        expect(calc.calculate('5 + 5')).to eql(10)
+      end
     end
-  end
 
-  context "given '5 * 4'" do
-    it "returns '20'" do
-      calc = Calc.new
-      expect(calc.calculate('5 * 4')).to eql(20)
+    context "given '5 - 4'" do
+      it "returns '1'" do
+        calc = Calc.new
+        expect(calc.calculate('5 - 4')).to eql(1)
+      end
     end
-  end
 
-  context "given '20 / 4'" do
-    it "returns '5'" do
-      calc = Calc.new
-      expect(calc.calculate('20 / 4')).to eql(5)
+    context "given '5 * 4'" do
+      it "returns '20'" do
+        expect(calc.calculate('5 * 4')).to eql(20)
+      end
     end
-  end
 
-  context "given '5+5'" do
-    it "returns '10'" do
-      calc = Calc.new
-      expect(calc.calculate('5+5')).to eql(10)
+    context "given '20 / 4'" do
+      it "returns '5'" do
+        expect(calc.calculate('20 / 4')).to eql(5)
+      end
+    end
+
+    context "given '5+5'" do
+      it "returns '10'" do
+        expect(calc.calculate('5+5')).to eql(10)
+      end
     end
   end
 end
